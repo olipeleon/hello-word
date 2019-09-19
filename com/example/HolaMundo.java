216c6e356d885c63ac312ee4a7421ea16cccec7c
olipeleon <olipeleon@yahoo.es>
2019-09-19 23:43:52 +0200

    Test commit desde VSCode

--- com/example/HolaMundo.java
+++ com/example/HolaMundo.java
@@ -6,5 +6,16 @@ public class HolaMundo
   public static void main(String[] pars)
   {
     System.out.println("Hola mundo 2!!");
+    System.out.println("Otra prueba");
+
+    System.out.println("Esto es otra prueba");
+
+    try {
+      System.out.println("Otra prueba mas");
+      throw new Exception();
+    } catch (Exception e) 
+    {
+      e.printStackTrace();
+    }
   }
 }
